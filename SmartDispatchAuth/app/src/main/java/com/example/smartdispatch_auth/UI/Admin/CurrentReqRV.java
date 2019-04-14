c9da99a0612e119c2fcf45efe567a6321092ece6
PratikRajani <38072414+PratikRajani@users.noreply.github.com>
2019-04-14 16:08:23 +0530

    Create CurrentReqRV.java

---
+++ SmartDispatchAuth/app/src/main/java/com/example/smartdispatch_auth/UI/Admin/CurrentReqRV.java
@@ -0,0 +1,78 @@
+package com.example.smartdispatch_auth.UI.Admin;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.view.View;
+
+import com.example.smartdispatch_auth.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class CurrentReqRV extends AppCompatActivity implements View.OnClickListener {
+
+    private android.support.v7.widget.RecyclerView recyclerView;
+    private CurrentRequestAdapter requestAdapter;
+    private android.support.v7.widget.RecyclerView.LayoutManager layoutManager;
+    String requestType;
+
+    List<CurrentRequest> requestList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_current_request);
+
+        Intent intent = getIntent();
+        requestType = intent.getStringExtra("requestType");
+
+        requestList = new ArrayList<>();
+
+        recyclerView = findViewById(R.id.crrecyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        requestList.add(
+                new CurrentRequest(
+                        "fhcgvhbjnk",
+                        "fcgvhbjnkm",
+                        "Pratik",
+                        "1546523542",
+                        "GJ5 RG 4565"
+                )
+        );
+        requestList.add(
+                new CurrentRequest(
+                        "fhcgvhbjnk",
+                        "fcgvhbjnkm",
+                        "Pratik",
+                        "1546523542",
+                        "GJ5 RG 4565"
+                )
+        );
+        requestList.add(
+                new CurrentRequest(
+                        "fhcgvhbjnk",
+                        "fcgvhbjnkm",
+                        "Pratik",
+                        "1546523542",
+                        "GJ5 RG 4565"
+                )
+        );
+
+        requestAdapter = new CurrentRequestAdapter(this, requestList);
+        recyclerView.setAdapter(requestAdapter);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
